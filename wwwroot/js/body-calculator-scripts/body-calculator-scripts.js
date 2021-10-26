a1f94257c09d78a4a9920660ffbf54c3160e48bb
shaplykon <shaplykon@gmail.com>
2021-10-26 20:43:14 +0300

    calculator implemented

---
+++ wwwroot/js/body-calculator-scripts/body-calculator-scripts.js
@@ -0,0 +1,23 @@
+﻿var HEIGHT_UNITS;
+var WEIGHT_UNITS;
+
+
+window.onload += () => {
+
+}
+
+function onWeightUnitsChanged(value) { WEIGHT_UNITS = value; }
+
+function onHeighttUnitsChanged(value) { HEIGHT_UNITS = value; }
+
+function weightInputChanged() {
+    var input = document.getElementById("weightInput");
+    var value = input.innerHTML;
+    if (!(/^[0-9]*[.,]?[0-9]+$/.test(value))) {
+        alert(1);
+    }
+}
+
+function heightInputChanged() {
+
+}
