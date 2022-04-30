db070e40dc816f8f09a9e3adcb06699f4c54bbb9
Nikita Shaplyko <shaplykon@gmail.com>
2022-04-30 20:50:02 +0300

    moved to .net 6.0 version

---
+++ Models/MapsCoordinates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthAndBeauty.Models
+{
+    [Table("google_maps")]
+    public class MapsCoordinates
+    {   
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("latitude")]
+        public double Latitude { get; set; }
+
+        [Column("longtitude")]
+        public double Longtitude { get; set; }
+
+        [Column("address_name")]
+        public string AddressName { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+    }
+}
